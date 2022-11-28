8532c8f3bb680af6c4baf8810815846f40825ba8
jonnyabrams <jonnyabrams@hotmail.com>
2022-11-28 09:37:45 +0000

    feat: logo in HomeScreen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,14 @@
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text className="text-red-500">HomeScreen</Text>
+    <SafeAreaView className="bg-white h-full">
+      <View className="p-5">
+        <Image
+          style={{ width: 100, height: 100, resizeMode: "contain" }}
+          source={require("../assets/logo.png")}
+        />
+      </View>
     </SafeAreaView>
   );
 };
