b53aca354d92e3a15bc5f2be7e088127b9afb362
jonnyabrams <jonnyabrams@hotmail.com>
2022-11-28 09:20:42 +0000

    feat: nativewind css

--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
 
 import { store } from "./store";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Ubre</Text>
-      </View>
+      <HomeScreen />
     </Provider>
   );
 };
---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text className="text-red-500">HomeScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
