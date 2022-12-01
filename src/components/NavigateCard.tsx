d19036aee66f881141e388ffb8dd5b0a3c99a629
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-01 14:27:20 +0000

    feat: setDestination in NavigateCard

--- src/components/NavigateCard.tsx
+++ src/components/NavigateCard.tsx
@@ -1,11 +1,65 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useAppDispatch } from "../hooks";
+import { setDestination } from "../slices/navSlice";
+import { useNavigation } from "@react-navigation/native";
 
 const NavigateCard = () => {
-  return (
-    <View>
-      <Text>NavigateCard</Text>
-    </View>
-  )
-}
+  const dispatch = useAppDispatch();
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
 
-export default NavigateCard
+  return (
+    <SafeAreaView className="flex-1 bg-white">
+      <Text className="py-5 text-xl text-center">Good Morning, Jonny</Text>
+      <View className="flex-shrink border-t border-gray-200">
+        <View>
+          <GooglePlacesAutocomplete
+            placeholder="Where to?"
+            styles={{
+              container: {
+                backgroundColor: "white",
+                paddingTop: 20,
+                flex: 0,
+              },
+              textInput: {
+                backgroundColor: "#DDDDDF",
+                borderRadius: 0,
+                fontSize: 18,
+              },
+              textInputContainer: {
+                paddingHorizontal: 20,
+                paddingBottom: 0,
+              },
+            }}
+            textInputProps={{
+              returnKeyType: "search",
+            }}
+            minLength={2}
+            onPress={(data, details = null) => {
+              dispatch(
+                setDestination({
+                  location: details?.geometry.location,
+                  description: data?.description,
+                })
+              );
+              navigation.navigate("RideOptionsCard");
+            }}
+            fetchDetails={true}
+            enablePoweredByContainer={false}
+            query={{
+              key: GOOGLE_MAPS_APIKEY,
+              language: "en",
+            }}
+            nearbyPlacesAPI="GooglePlacesSearch"
+            debounce={400}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default NavigateCard;
