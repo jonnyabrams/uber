0443f571d340619003e9156f0893af2df4cb02a6
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-04 08:21:48 +0000

    feat: header and 'back' chevron in top of RideOptionsCard

--- src/components/RideOptionsCard.tsx
+++ src/components/RideOptionsCard.tsx
@@ -1,11 +1,17 @@
-import { View, Text } from 'react-native'
+import { Icon } from "@rneui/themed";
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
 
 const RideOptionsCard = () => {
   return (
-    <View>
-      <Text>RideOptionsCard</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="flex-grow bg-white">
+      <View>
+        <TouchableOpacity className="absolute p-3 rounded-full top-3 left-5">
+          <Icon name="chevron-left" type="fontawesome" />
+        </TouchableOpacity>
+        <Text className="py-5 text-xl text-center">Select a Ride</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default RideOptionsCard
+export default RideOptionsCard;
