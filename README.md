62ec2d7d384e8ff2e649dbcd41dadb3eca6f9c19
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-04 13:57:52 +0000

    feat: dispatch travel time info

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Ubre
 
-An Uber clone (pronounced 'Ooh-bray') using React Native, TypeScript, Redux Toolkit, Tailwind CSS, Google Maps API and Google Places Autocomplete.
+An Uber clone (pronounced 'Ooh-bray') using React Native, TypeScript, Redux Toolkit, Tailwind CSS, Google Maps API, Google Places Autocomplete and Google Distance Matrix.
 
 Users can select an origin and a destination, both with autocomplete inputs, before being taken to a screen showing the directions on a map with a choice of rides to select from.
 
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,14 +3,15 @@ import MapView, { Marker } from "react-native-maps";
 import MapViewDirections from "react-native-maps-directions";
 import { useEffect, useRef } from "react";
 
-import { useAppSelector } from "../hooks";
-import { selectDestination, selectOrigin } from "../slices/navSlice";
+import { useAppDispatch, useAppSelector } from "../hooks";
+import { selectDestination, selectOrigin, setTravelTimeInfo } from "../slices/navSlice";
 import { GOOGLE_MAPS_APIKEY } from "@env";
 
 const Map = () => {
   const origin = useAppSelector(selectOrigin);
   const destination = useAppSelector(selectDestination);
   const mapRef = useRef<any>(null);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     // if no origin or destination, immediately return - ie. carry on
@@ -21,6 +22,22 @@ const Map = () => {
       edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
     });
   }, [origin, destination]);
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+
+    const getTravelTime = async () => {
+      const URL = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin.description}&destinations=${destination.description}&key=${GOOGLE_MAPS_APIKEY}`;
+
+      fetch(URL)
+        .then((res) => res.json())
+        .then((data) => {
+          dispatch(setTravelTimeInfo(data.rows[0].elements[0]))
+        });
+    };
+
+    getTravelTime();
+  }, [origin, destination, GOOGLE_MAPS_APIKEY]);
 
   return (
     <MapView
