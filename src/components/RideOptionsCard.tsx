0a82ff64d60b4ae2a8265d13c55fdf4a90ba53cd
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-04 10:01:35 +0000

    feat: navigate back onPress in RideOptions chevron (doesn't work)

--- src/components/RideOptionsCard.tsx
+++ src/components/RideOptionsCard.tsx
@@ -1,11 +1,18 @@
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { Icon } from "@rneui/themed";
 import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
 
 const RideOptionsCard = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
+
   return (
     <SafeAreaView className="flex-grow bg-white">
       <View>
-        <TouchableOpacity className="absolute p-3 rounded-full top-3 left-5">
+        <TouchableOpacity
+          onPress={() => navigation.navigate("NavigateCard")}
+          className="absolute p-3 rounded-full top-3 left-5"
+        >
           <Icon name="chevron-left" type="fontawesome" />
         </TouchableOpacity>
         <Text className="py-5 text-xl text-center">Select a Ride</Text>
