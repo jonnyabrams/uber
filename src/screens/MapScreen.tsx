ba691e2f8ddd7a948e63b51901132b68e39a2822
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-04 14:34:30 +0000

    feat: floating menu button to return to HomeScreen

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,5 +1,10 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { View, Text } from "react-native";
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from "@react-navigation/native-stack";
+import { View, Text, TouchableOpacity } from "react-native";
+import { Icon } from "@rneui/themed";
+import { useNavigation } from "@react-navigation/native";
 
 import Map from "../components/Map";
 import NavigateCard from "../components/NavigateCard";
@@ -7,9 +12,17 @@ import RideOptionsCard from "../components/RideOptionsCard";
 
 const MapScreen = () => {
   const Stack = createNativeStackNavigator();
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
 
   return (
     <View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("HomeScreen")}
+        className="absolute z-50 p-3 bg-gray-100 rounded-full shadow-lg top-16 left-8"
+      >
+        <Icon name="menu" />
+      </TouchableOpacity>
+
       <View className="h-1/2">
         <Map />
       </View>
